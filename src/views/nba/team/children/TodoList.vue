e94fffe16125a51d4707216e15168e279bcc2ecc
geekftz <472849306@qq.com>
2019-11-26 22:07:00 +0800

    push

---
+++ src/views/nba/team/children/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="todo-list">
+        <div v-for="(item, index) in item" :key="index">
+            {{
+                item
+            }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'todo-list',
+
+        data() {
+            return {
+                
+            }
+        },
+
+        props: {
+            item: {
+                type:Array,
+                default: () => []
+            }
+        },
+
+        mounted() {
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
